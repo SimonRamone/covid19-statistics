bd0f6f7a8653de597fcb014092760795a7e703b0
SimonRamone <sramonas@yahoo.com>
2021-02-12 01:02:49 +0200

    Project created

---
+++ src/main/java/lt/simonasramonas/covid19statistics/Covid19StatisticsApplication.java
@@ -0,0 +1,13 @@
+package lt.simonasramonas.covid19statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19StatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19StatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/simonasramonas/covid19statistics/Covid19StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.simonasramonas.covid19statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19StatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
